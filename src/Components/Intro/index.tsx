2774e44d67cdacad01eafa1e7b55823ca906a372
GTM35 <035guuh@gmail.com>
2025-02-18 12:11:16 -0300

    :sparkles: feat: Sessão de intro

--- src/Components/Intro/index.tsx
+++ src/Components/Intro/index.tsx
@@ -1,18 +1,57 @@
+import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
+import {
+  ContainerIcon,
+  ImageHero,
+  Information,
+  IntroContainer,
+  Item,
+  Items,
+  TitleInfo,
+} from "./style";
+import { defaultTheme } from "../../styles/themes/default";
+
 export function Intro() {
   return (
     <IntroContainer>
-      <Intro>
-        <Info>
-          <h1></h1>
-          <p></p>
-        </Info>
-        <div>
-          <div>items</div>
-          <div>items</div>
-          <div>items</div>
-          <div>items</div>
-        </div>
-      </Intro>
+      <Information>
+        <TitleInfo>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+        </TitleInfo>
+        <Items>
+          <Item>
+            <ContainerIcon color={defaultTheme["yellow-dark"]}>
+              <ShoppingCart size={16} weight="fill" />
+            </ContainerIcon>
+            <span>Compra simples e segura</span>
+          </Item>
+
+          <Item>
+            <ContainerIcon color={defaultTheme["base-text"]}>
+              <Package size={16} weight="fill" />
+            </ContainerIcon>
+            <span>Embalagem mantém o café intacto</span>
+          </Item>
+
+          <Item>
+            <ContainerIcon color={defaultTheme.yellow}>
+              <Timer size={16} weight="fill" />
+            </ContainerIcon>
+            <span>Entrega rápida e rastreada</span>
+          </Item>
+          <Item>
+            <ContainerIcon color={defaultTheme.purple}>
+              <Coffee size={16} weight="fill" />
+            </ContainerIcon>
+            <span>O café chega fresquinho até você</span>
+          </Item>
+        </Items>
+      </Information>
+
+      <ImageHero src="./coffe.svg" />
     </IntroContainer>
   );
 }
--- src/Components/Intro/style.ts
+++ src/Components/Intro/style.ts
@@ -1,11 +1,84 @@
 import styled from "styled-components";
+import { textL, textM, titleXL } from "../../styles/fonts";
 
-export const IntroContainer = styled.div``;
+export interface IconContainerProps {
+  color: string;
+}
 
-export const Intro = styled.div``;
+export const IntroContainer = styled.section`
+  height: 34rem;
 
-export const Info = styled.div``;
+  display: flex;
+  padding: 0 10rem;
+  align-items: center;
+  justify-content: space-between;
 
-export const Items = styled.div``;
+  background-image: url("../background.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 
-export const Item = styled.div``;
+export const Information = styled.div`
+  width: 100%;
+  max-width: 36.75rem;
+`;
+
+export const TitleInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  h1 {
+    ${titleXL}
+  }
+
+  p {
+    width: 100%;
+    ${textL({ weight: "400" })};
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+`;
+
+export const Items = styled.div`
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 1.25rem;
+  margin-top: 4.125rem;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  width: fit-content;
+
+  span {
+    ${textM({ weight: "400" })}
+  }
+`;
+
+export const ImageHero = styled.img`
+  width: 100%;
+  max-width: 29.75rem;
+`;
+
+export const ContainerIcon = styled.div<IconContainerProps>`
+  padding: 0.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+
+  svg {
+    color: #fff;
+  }
+
+  ${(props) => {
+    return `
+      background-color: ${props.color};
+    `;
+  }};
+`;
